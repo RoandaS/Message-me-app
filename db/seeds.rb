69eb4aa7a48d959b61de19e00967f84155d0cc8a
RoandaS <roanda138@gmail.com>
2024-11-28 18:56:12 +0200

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Roanda", password: "password123")
+User.create(username: "Donovan", password: "password123")
+User.create(username: "Lilly", password: "password123")
+User.create(username: "Eden", password: "password123")
+User.create(username: "Jesus", password: "password123")
